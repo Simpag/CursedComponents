da5941eb437062798a086a74925458f937e70c2c
Abdel Ahmed <abdu1999@gmx.net>
2024-08-10 18:32:33 +0200

    feat: enchant sold items of lucky villager

--- src/main/java/com/ccteam/cursedcomponents/villager/CustomVillagerManager.java
+++ src/main/java/com/ccteam/cursedcomponents/villager/CustomVillagerManager.java
@@ -1,10 +1,13 @@
 package com.ccteam.cursedcomponents.villager;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.item.trading.MerchantOffers;
@@ -15,8 +18,6 @@ public class CustomVillagerManager {
     }
 
     public static Villager createLuckyVillager(Level world) {
-        // TODO: sell enchanted tools instead
-
         Villager villager = EntityType.VILLAGER.create(world);
         if (villager == null) {
             return null;
@@ -24,12 +25,16 @@ public class CustomVillagerManager {
         villager.setVillagerData(villager.getVillagerData().setProfession(VillagerProfession.MASON));
         villager.setVillagerXp(10000);
 
-        ItemStack sword = new ItemStack(Items.DIAMOND_SWORD.asItem(), 2);
+        ItemStack sword = new ItemStack(Items.DIAMOND_SWORD.asItem(), 1);
+
+        EnchantmentHelper.enchantItem(world.random, sword, 30, world.registryAccess(),
+                world.registryAccess().registryOrThrow(Registries.ENCHANTMENT)
+                        .getTag(EnchantmentTags.DAMAGE_EXCLUSIVE));
 
         MerchantOffer offer = new MerchantOffer(
                 new ItemCost(Items.WHEAT, 16),
                 sword,
-                1, 9, 0.05f
+                2, 9, 0.05f
         );
 
         MerchantOffers offers = new MerchantOffers();

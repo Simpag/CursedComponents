27e128ed593a99a8c939be046646c2331fd26ecd
Abdel Ahmed <abdu1999@gmx.net>
2024-08-13 21:46:57 +0200

    feat: make lucky parrot flying and give player speedboost

--- src/main/java/com/ccteam/cursedcomponents/entity/custom/LuckyParrot.java
+++ src/main/java/com/ccteam/cursedcomponents/entity/custom/LuckyParrot.java
@@ -1,6 +1,7 @@
 package com.ccteam.cursedcomponents.entity.custom;
 
-import com.ccteam.cursedcomponents.entity.ModEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -8,42 +9,60 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.ai.util.LandRandomPos;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.FlyingAnimal;
+import net.minecraft.world.entity.animal.ShoulderRidingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
-public class LuckyParrot extends Animal {
+public class LuckyParrot extends ShoulderRidingEntity implements FlyingAnimal {
     private static final EntityDataAccessor<Integer> VARIANT =
             SynchedEntityData.defineId(LuckyParrot.class, EntityDataSerializers.INT);
 
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
+    public float flap;
+    public float flapSpeed;
+    public float oFlapSpeed;
+    public float oFlap;
+    private float flapping = 1.0F;
+    private float nextFlap = 1.0F;
 
-    public LuckyParrot(EntityType<? extends Animal> pEntityType, Level pLevel) {
+    public LuckyParrot(EntityType<? extends ShoulderRidingEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     @Override
     protected void registerGoals() {
+        this.goalSelector.addGoal(0, new TamableAnimal.TamableAnimalPanicGoal(1.25));
         this.goalSelector.addGoal(0, new FloatGoal(this));
-
-        this.goalSelector.addGoal(1, new PanicGoal(this, 2.0));
-        this.goalSelector.addGoal(2, new BreedGoal(this, 1.0));
-
-        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25));
-
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
-        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
-        this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(2, new SitWhenOrderedToGoal(this));
+        this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 1.0, 5.0F, 1.0F));
+        this.goalSelector.addGoal(3, new LandOnOwnersShoulderGoal(this));
+        this.goalSelector.addGoal(3, new FollowMobGoal(this, 1.0, 3.0F, 7.0F));
+        this.goalSelector.addGoal(4, new ParrotWanderGoal(this, 1.0));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
@@ -54,14 +73,98 @@ public class LuckyParrot extends Animal {
     }
 
     @Override
+    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+        if (!this.level().isClientSide) {
+            if (this.isTame()) {
+                this.setOrderedToSit(!this.isOrderedToSit());
+            } else {
+                this.tame(player);
+                this.level().broadcastEntityEvent(this, (byte)7);
+            }
+        } else {
+            this.level().broadcastEntityEvent(this, (byte)6);
+        }
+        return InteractionResult.sidedSuccess(this.level().isClientSide);
+    }
+
+    @Override
+    public boolean isBaby() {
+        return false;
+    }
+
+    @Override
+    public boolean canBeLeashed() {
+        return true;
+    }
+
+    @Override
     public boolean isFood(ItemStack pStack) {
         return false;
+    }
+
+    @Override
+    public boolean canMate(Animal otherAnimal) {
+        return false;
+    }
+
+    @Override
+    public Vec3 getLeashOffset() {
+        return new Vec3(0.0, (double)(0.5F * this.getEyeHeight()), (double)(this.getBbWidth() * 0.4F));
+    }
+
+    static class ParrotWanderGoal extends WaterAvoidingRandomFlyingGoal {
+        public ParrotWanderGoal(PathfinderMob p_186224_, double p_186225_) {
+            super(p_186224_, p_186225_);
+        }
+
+        @javax.annotation.Nullable
+        @Override
+        protected Vec3 getPosition() {
+            Vec3 vec3 = null;
+            if (this.mob.isInWater()) {
+                vec3 = LandRandomPos.getPos(this.mob, 15, 15);
+            }
+
+            if (this.mob.getRandom().nextFloat() >= this.probability) {
+                vec3 = this.getTreePos();
+            }
+
+            return vec3 == null ? super.getPosition() : vec3;
+        }
+
+        @javax.annotation.Nullable
+        private Vec3 getTreePos() {
+            BlockPos blockpos = this.mob.blockPosition();
+            BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+            BlockPos.MutableBlockPos blockpos$mutableblockpos1 = new BlockPos.MutableBlockPos();
+
+            for (BlockPos blockpos1 : BlockPos.betweenClosed(
+                    Mth.floor(this.mob.getX() - 3.0),
+                    Mth.floor(this.mob.getY() - 6.0),
+                    Mth.floor(this.mob.getZ() - 3.0),
+                    Mth.floor(this.mob.getX() + 3.0),
+                    Mth.floor(this.mob.getY() + 6.0),
+                    Mth.floor(this.mob.getZ() + 3.0)
+            )) {
+                if (!blockpos.equals(blockpos1)) {
+                    BlockState blockstate = this.mob.level().getBlockState(blockpos$mutableblockpos1.setWithOffset(blockpos1, Direction.DOWN));
+                    boolean flag = blockstate.getBlock() instanceof LeavesBlock || blockstate.is(BlockTags.LOGS);
+                    if (flag
+                            && this.mob.level().isEmptyBlock(blockpos1)
+                            && this.mob.level().isEmptyBlock(blockpos$mutableblockpos.setWithOffset(blockpos1, Direction.UP))) {
+                        return Vec3.atBottomCenterOf(blockpos1);
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel pLevel, AgeableMob pOtherParent) {
-        return ModEntities.LUCKY_PARROT.get().create(pLevel);
+        return null;
     }
 
     private void setupAnimationStates() {
@@ -80,9 +183,47 @@ public class LuckyParrot extends Animal {
         if(this.level().isClientSide()) {
             this.setupAnimationStates();
         }
+
+        if (!this.level().isClientSide && this.getOwner() != null) {
+            Player player = (Player) this.getOwner();
+            if (player != null && !player.hasEffect(MobEffects.MOVEMENT_SPEED)) {
+                player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 100, 10, false, true));
+            }
+        }
     }
 
-    /* VARIANT */
+    @Override
+    protected PathNavigation createNavigation(Level level) {
+        FlyingPathNavigation flyingpathnavigation = new FlyingPathNavigation(this, level);
+        flyingpathnavigation.setCanOpenDoors(false);
+        flyingpathnavigation.setCanFloat(true);
+        flyingpathnavigation.setCanPassDoors(true);
+        return flyingpathnavigation;
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        this.calculateFlapping();
+    }
+
+    private void calculateFlapping() {
+        this.oFlap = this.flap;
+        this.oFlapSpeed = this.flapSpeed;
+        this.flapSpeed = this.flapSpeed + (float)(!this.onGround() && !this.isPassenger() ? 4 : -1) * 0.3F;
+        this.flapSpeed = Mth.clamp(this.flapSpeed, 0.0F, 1.0F);
+        if (!this.onGround() && this.flapping < 1.0F) {
+            this.flapping = 1.0F;
+        }
+
+        this.flapping *= 0.9F;
+        Vec3 vec3 = this.getDeltaMovement();
+        if (!this.onGround() && vec3.y < 0.0) {
+            this.setDeltaMovement(vec3.multiply(1.0, 0.6, 1.0));
+        }
+
+        this.flap = this.flap + this.flapping * 2.0F;
+    }
 
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder pBuilder) {
@@ -92,6 +233,11 @@ public class LuckyParrot extends Animal {
 
     private int getTypeVariant() {
         return this.entityData.get(VARIANT);
+    }
+
+    @Override
+    protected boolean canFlyToOwner() {
+        return true;
     }
 
     @Override
@@ -130,5 +276,10 @@ public class LuckyParrot extends Animal {
     @Override
     protected SoundEvent getDeathSound() {
         return SoundEvents.PARROT_DEATH;
+    }
+
+    @Override
+    public boolean isFlying() {
+        return !this.onGround();
     }
 }

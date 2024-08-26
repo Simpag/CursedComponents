861c067776ce5f6cdea0f60e28d2f491916648a6
Simpag <simon.granstrom@gmail.com>
2024-08-26 02:03:23 +0200

    feat: spike hitbox, xp drop and config

--- src/main/java/com/ccteam/cursedcomponents/block/custom/SpikeBlock.java
+++ src/main/java/com/ccteam/cursedcomponents/block/custom/SpikeBlock.java
@@ -1,34 +1,36 @@
 package com.ccteam.cursedcomponents.block.custom;
 
+import com.ccteam.cursedcomponents.Config;
+import com.ccteam.cursedcomponents.util.VoxelShapeUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RedstoneTorchBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SpikeBlock extends Block {
+    public static final VoxelShape SHAPE = VoxelShapeUtils.combineShapes(
+            box(0, 0, 0, 16, 2, 16)
+    );
     public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
 
     public SpikeBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.defaultBlockState().setValue(LIT, Boolean.valueOf(false)));
+        this.registerDefaultState(this.defaultBlockState().setValue(LIT, Boolean.FALSE));
     }
 
     @Override
@@ -37,7 +39,8 @@ public class SpikeBlock extends Block {
     }
 
     @Override
-    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+    protected void neighborChanged(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Block block, @NotNull BlockPos fromPos, boolean isMoving) {
+        super.neighborChanged(state, level, pos, block, fromPos, isMoving);
         if (!level.isClientSide) {
             boolean flag = state.getValue(LIT);
             if (flag != level.hasNeighborSignal(pos)) {
@@ -51,28 +54,37 @@ public class SpikeBlock extends Block {
     }
 
     @Override
-    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+    protected void tick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
         if (state.getValue(LIT) && !level.hasNeighborSignal(pos)) {
             level.setBlock(pos, state.cycle(LIT), 2);
         }
     }
 
     @Override
-    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
-        if (!state.getValue(BlockStateProperties.LIT))
+    protected void entityInside(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Entity entity) {
+        if (level.isClientSide || !state.getValue(BlockStateProperties.LIT))
             return;
 
         if (entity instanceof LivingEntity livingEntity) {
             if (entity.isShiftKeyDown())
                 return;
 
-            livingEntity.hurt(livingEntity.damageSources().generic(), this.getDamagePerTick());
+            livingEntity.hurt(livingEntity.damageSources().generic(), this.getDamage());
+
+            if (this.shouldDropXp() && livingEntity.isDeadOrDying()) {
+                ServerLevel serverLevel = (ServerLevel) level;
+                int reward = net.neoforged.neoforge.event.EventHooks.getExperienceDrop(livingEntity, null, livingEntity.getExperienceReward(serverLevel, null));
+                ExperienceOrb.award(serverLevel, pos.getCenter(), reward);
+            }
         }
     }
 
-    private float getDamagePerTick() {
-        //return Config.spikeDamagePerTick.get();
-        return 2f;
+    private float getDamage() {
+        return Config.spikeDamage.floatValue();
+    }
+
+    private boolean shouldDropXp() {
+        return Config.spikeDropXp;
     }
 
     @Override
@@ -81,7 +93,7 @@ public class SpikeBlock extends Block {
     }
 
     @Override
-    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
-        return true;
+    protected @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        return SHAPE;
     }
 }
